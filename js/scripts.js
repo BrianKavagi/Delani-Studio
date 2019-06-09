af7c1e75324a31c811bbb8086b1752d593ebafa1
BrianKavagi <briankavagi06@gmail.com>
2019-06-09 20:35:00 +0300

    Design

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("/home/student/Desktop/IP3/assets/services_icons/design_icon.png").click(function() {
+      $(".hide0").slideToggle(1000);
+      $(".show0").slideToggle(1200);
+    }
+}
