d3bdf3b7f420652cd0e6090d45290aabfc0e8302
BrianKavagi <briankavagi06@gmail.com>
2019-06-10 09:13:59 +0300

    jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
-$(document).ready(function() {
-    $("/home/student/Desktop/IP3/assets/services_icons/design_icon.png").click(function() {
-      $("p-show").toggle();
-      $("p-hide").toggle();
-    }
-}
+$(document).ready(function () {
+    $(".WHAT WE DO p").hide();
+ 
+    $("/home/student/Desktop/IP3/assets/services_icons/design_icon.png").click(function () {
+        $(".p1").toggle();
+ 
+    });
+ });
+ 
+ $(document).ready(function () {
+    $("/home/student/Desktop/IP3/assets/services_icons/dev_icon.png").click(function () {
+        $(".p2").toggle();
+ 
+    });
+ });
+ $(document).ready(function () {
+    $("/home/student/Desktop/IP3/assets/services_icons/product_icon.png").click(function () {
+        $(".p3").toggle();
+ 
+    });
+ });
